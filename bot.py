e4a57c9f2f8d7a2c41580b54b9342bcf9395c5da
test00123 <109013696+test00123@users.noreply.github.com>
2022-12-10 22:54:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,12 @@
 import asyncio
 from pyrogram import Client, compose,idle
+
 import os
+from aiohttp import web
+from os import environ 
+PORT = environ.get("PORT", "8080"
+
+
 
 from plugins.cb_data import app as Client2
 
@@ -33,6 +39,25 @@ if STRING:
     idle()
     for app in apps:
         app.stop()
+    app = web.AppRunner(await web_server())
+    await app.setup()
+    bind_address = "0.0.0.0"
+    await web.TCPSite(app, bind_address, PORT).start()
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
+routes = web.RouteTableDef()
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("testbot")
+
+       
+
+
+
     
 else:
     bot.run()
